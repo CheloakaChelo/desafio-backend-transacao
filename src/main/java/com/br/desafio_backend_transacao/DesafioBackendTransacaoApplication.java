3919a3c87d44af624166da630b1a757b50b0a07b
chelo <jmjoao8@hotmail.com>
2026-02-03 19:54:18 -0300

    first commit

---
+++ src/main/java/com/br/desafio_backend_transacao/DesafioBackendTransacaoApplication.java
@@ -0,0 +1,13 @@
+package com.br.desafio_backend_transacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackendTransacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackendTransacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/desafio_backend_transacao/DesafioBackendTransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.desafio_backend_transacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackendTransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
